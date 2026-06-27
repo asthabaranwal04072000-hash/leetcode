dc8d9a62fd8e11d2d3922bfb455076bddfb20830
Astha Baranwal <asthabaranwal04072000@gmail.com>
2026-06-27 16:36:56 +0530

    feat: solve Two Sum using hashmap

---
+++ arrays/two_sum.py
@@ -0,0 +1,10 @@
+def two_sum(nums,target):
+    need = 0
+    seen = {}
+    for i,num in enumerate(nums):
+        need = target - num
+        if need in seen:
+            return [seen[need],i]
+        seen[num] = i
+    
+print(two_sum([2,7,11,15],9))
