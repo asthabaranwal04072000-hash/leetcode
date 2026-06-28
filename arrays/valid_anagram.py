e78193fded05fbbc39120291a404a6b4fec05bcb
Astha Baranwal <asthabaranwal04072000@gmail.com>
2026-06-28 17:43:53 +0530

    Update valid_anagram.py

--- arrays/valid_anagram.py
+++ arrays/valid_anagram.py
@@ -1,11 +1,20 @@
-def val_anagram(s,t):
+def val_anagram(s, t):
     if len(s) != len(t):
         return False
+
     count = {}
+
     for ch in s:
-        count[ch] = count.get(ch, 0) + 1 ##key,default
+        count[ch] = count.get(ch, 0) + 1
+
     for ch in t:
-        if ch in count:
-            count[ch] -= 1
-    return not count
+        if ch not in count:
+            return False
+
+        count[ch] -= 1
+
+        if count[ch] < 0:
+            return False
+
+    return True
 print(val_anagram("tan","pan"))
